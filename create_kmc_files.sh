bacaf7cb0abd7f4853f1f443ba4dd218bf8607ef
matthijs Brouwer <matthijs@brouwer.info>
2022-07-07 08:58:51 +0200

    update

--- create_kmc_files.sh
+++ create_kmc_files.sh
@@ -4,6 +4,8 @@ LOCATION_READFILES=data/reads/full
 LOCATION_KMERDATABASE=data/index/full
 LOCATION_TMP=tmp
 
+KMC=kmc
+
 THREADS=20
 KMER_SIZE=31
 MINIMUM_FREQ=2
@@ -16,7 +18,7 @@ for i in $({ cd "$LOCATION_READFILES" && find . -type d; }); do
             echo "$i"
             mkdir -p "$LOCATION_KMERDATABASE"/"$i"
             find "$LOCATION_READFILES"/"$i" -name *.fastq.gz > "$LOCATION_KMERDATABASE"/"$i/kmer.lst"
-            kmc -k"$KMER_SIZE" -ci"$MINIMUM_FREQ" \
+            eval $KMC -k"$KMER_SIZE" -ci"$MINIMUM_FREQ" \
                -cs"$MAXIMUM_FREQ" -t"$THREADS" \
                @"$LOCATION_KMERDATABASE"/"$i/kmer.lst" "$LOCATION_KMERDATABASE"/"$i/kmer.kmc" "$LOCATION_TMP"
             rm "$LOCATION_KMERDATABASE"/"$i/kmer.lst"
